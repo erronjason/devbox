6657ba15dde3ea2262e920c4dfa679d514549cbd
Jason Sayre <ejsayre@gmail.com>
2015-03-29 22:09:56 -0400

    Further work on the environment, stubbed out setup

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 A vagrant box for development in nonstandard or 
 unsetup environments (i.e. after a reload on on Windows)
+
+## Usage
+
+Install [Vagrant](https://www.vagrantup.com/downloads.html) and [VirtualBox](https://www.virtualbox.org/wiki/Downloads), then simply run `vagrant up` in the directory.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+##################################
+# Initial installation and upgrade
 apt-get update
-#apt-get upgrade
+apt-get upgrade
 
 # Set password for vagrant user for visual login
-echo vagrant:dev | chpasswd
+echo vagrant:dev | chpasswd # Initial password is after the colon
+userdel ubuntu
 
+################################
+# Install your development stack
+apt-get -y install python34 python-pip
+
+pip install virtualenvwrapper
+
+cat >> /home/vagrant/
+
+# Install a gui. This can be replaced by whatever you prefer
 apt-get -y install --no-install-recommends lubuntu-desktop
+DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt-get -q -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
+shutdown -r now
